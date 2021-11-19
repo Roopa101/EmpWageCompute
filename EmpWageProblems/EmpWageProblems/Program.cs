9271f3cc0c0e9a7407a4736fe30fda99407d3f32
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-19 23:04:17 +0530

    [Roopa] Calculating Wages till condition of working Hrs100 or 20 Days

--- EmpWageProblems/EmpWageProblems/Program.cs
+++ EmpWageProblems/EmpWageProblems/Program.cs
@@ -12,16 +12,19 @@ namespace EmpWageProblems
         public const int IS_PART_TIME = 2;
         public const int ratePerHrs = 20;
         public const int NO_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
         public static void Main(string[] args)
         {
 
-            int empWage = 0;
             int empHrs = 0;
-            int totalEmpWage = 0;
-            string typeOfEmp = "";
+            int totalEmpHrs = 0;
 
-            for (int day = 0; day <= NO_OF_WORKING_DAYS; day++)
+            int totalWorkingDays = 0;
+
+
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int randomInput = random.Next(0, 3);
 
@@ -30,21 +33,24 @@ namespace EmpWageProblems
                     case IS_FULL_TIME:
                         empHrs = 8;
                         break;
+
                     case IS_PART_TIME:
                         empHrs = 4;
                         break;
+
                     default:
                         empHrs = 0;
                         break;
                 }
 
-                empWage = ratePerHrs * empHrs;
-                Console.WriteLine("{0} Emp of the day {1} Wage is {2} ", typeOfEmp, day, empWage);
-                totalEmpWage = totalEmpWage + empWage;
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:" + totalWorkingDays + "EmpprHr:" + empHrs);
+
             }
-            Console.WriteLine("Employee Wage for {0} day is :{1}", NO_OF_WORKING_DAYS, totalEmpWage);
+
+            int totalEmpWage = totalEmpHrs * ratePerHrs;
+            Console.WriteLine("total Employee wage is :" + totalEmpWage);
             Console.ReadLine();
         }
     }
 }
-
